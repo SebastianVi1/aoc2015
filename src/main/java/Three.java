fbc33a293cae13015edb686e9619a6d6737154c3
Sebastian <sebastianvh86@gmail.com>
2025-01-02 22:07:33 -0600

    Day 3 part 2 - solved

--- src/main/java/Three.java
+++ src/main/java/Three.java
@@ -5,25 +5,40 @@ import java.util.List;
 public class Three {
 
     public int solve(List<String> lines, int part){
-        int houseCounter = 1;
-        int[] actualCordenates = {0,0};
+        int[] santaActualCordenates = {0,0};
+        int[] roboSantaActualCordenates = {0,0};
+
         HashSet<String> cordenates = new HashSet<String>();
-        cordenates.add(Arrays.toString(actualCordenates));
+        cordenates.add(Arrays.toString(santaActualCordenates));
+
+
         String chain = lines.toString();
         for (int i = 0; i < chain.length(); i++){
             char arrow = chain.charAt(i);
-            if (arrow == '^') {
-                actualCordenates[1] += 1;
+            if ((i+ 1) % 2 == 0 && arrow == '^') {
+                santaActualCordenates[1] += 1;
             }
-            else if (arrow == 'v'){
-                actualCordenates[1] -= 1;
-            } else if (arrow == '<') {
-                actualCordenates[0] -= 1;
-            } else if (arrow == '>') {
-                actualCordenates[0] += 1;
+            else if ((i+1) % 2 == 0 && arrow == 'v'){
+                santaActualCordenates[1] -= 1;
+            } else if ((i+1) % 2 == 0 && arrow == '<') {
+                santaActualCordenates[0] -= 1;
+            } else if ((i+1) % 2 == 0 && arrow == '>') {
+                santaActualCordenates[0] += 1;
             }
 
-            cordenates.add(Arrays.toString(actualCordenates));
+            if ((i+1) % 2 != 0 && arrow == '^') {
+                roboSantaActualCordenates[1] += 1;
+            }
+            else if ((i+1) % 2 != 0 && arrow == 'v'){
+                roboSantaActualCordenates[1] -= 1;
+            } else if ((i+1) % 2 != 0 && arrow == '<') {
+                roboSantaActualCordenates[0] -= 1;
+            } else if ((i+1) % 2 != 0 && arrow == '>') {
+                roboSantaActualCordenates[0] += 1;
+            }
+
+            cordenates.add(Arrays.toString(santaActualCordenates));
+            cordenates.add(Arrays.toString(roboSantaActualCordenates));
         }
 
      return cordenates.size();
